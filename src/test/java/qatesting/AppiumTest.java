603ab490f28f1793db5402d4b42ae7e2e6073147
patelhaniben369 <patelhaniben369@gmail.com>
2019-04-26 09:50:12 -0400

    commit first

---
+++ src/test/java/qatesting/AppiumTest.java
@@ -0,0 +1,17 @@
+package qatesting;
+
+import org.testng.annotations.Test;
+
+public class AppiumTest {
+
+	@Test
+	public void NativeAppAndroid() {
+		System.out.println("NativeAppAndroidt");
+	}
+	
+	@Test
+	public void IOSApps() {
+		System.out.println("IOSApps");
+	}
+	
+}
---
+++ src/test/java/qatesting/RESTAPITest.java
@@ -0,0 +1,16 @@
+package qatesting;
+
+import org.testng.annotations.Test;
+
+public class RESTAPITest {
+
+	@Test
+	public void postJira() {
+		System.out.println("postJira");
+	}
+	
+	@Test
+	public void deletTwitter() {
+		System.out.println("deletTwitter");
+	}
+}
